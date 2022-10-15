4ac0dcf78c3ad8ac60af798e592114c5340d75e7
Maria Romero <maria.romero@itech.media>
2022-10-15 10:39:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NotFound from "./pages/NotFound/NotFound";
 import Splash from "./pages/Splash/Splash";
 import "./App.css";
+import Create from "./pages/Create/Create";
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
           <Route exact path="/">
             <Splash />
           </Route>
-          {/* Catch all unmatched routes */}
+          <Route exact path="/create">
+            <Create />
+          </Route>
           <Route path="/">
             <NotFound />
           </Route>
